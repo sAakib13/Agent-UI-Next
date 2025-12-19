a7a4ceb313409773a5ee641b3cd7ae7cfaf25b37
sAakib13 <aakib@telerivet.com>
2025-12-19 11:44:14 +0545

    fix: update in the auth

--- lib/supbase.ts
+++ lib/supbase.ts
@@ -1,3 +1,4 @@
+// lib/supabase.ts
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
@@ -18,9 +19,7 @@ export async function createClient() {
               cookieStore.set(name, value, options)
             );
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // Ignored for Server Components
           }
         },
       },
